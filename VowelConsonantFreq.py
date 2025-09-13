a4424b8f5a0d593c9aec72179daba893e0259a67
Pavitra Subhasri <141206259+PavitraSubhasri15@users.noreply.github.com>
2025-09-13 12:26:48 +0530

    Create VowelConsonantFreq.py

---
+++ VowelConsonantFreq.py
@@ -0,0 +1,25 @@
+QUESTION:You are given a string s consisting of lowercase English letters ('a' to 'z').
+
+Your task is to:
+
+Find the vowel (one of 'a', 'e', 'i', 'o', or 'u') with the maximum frequency.
+Find the consonant (all other letters excluding vowels) with the maximum frequency.
+Return the sum of the two frequencies.
+
+Note: If multiple vowels or consonants have the same maximum frequency, you may choose any one of them. If there are no vowels or no consonants in the string, consider their frequency as 0.
+
+The frequency of a letter x is the number of times it occurs in the string.
+
+from collections import Counter
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        freq=Counter(s)
+        vowels="aeiou"
+        max1=0
+        max2=0
+        for ch in s:
+            if ch in vowels:
+                max1=max(max1,freq[ch])
+            else:
+                max2=max(max2,freq[ch])
+        return max1+max2
